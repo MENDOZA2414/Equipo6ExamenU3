41d5fd243d0e1baec7d1d70f354c3e65df3b7107
José Miguel <jmendoza_21@alu.uabcs.mx>
2023-05-05 22:59:43 -0600

    Ventana

--- ExamenU3/src/Ventana.java
+++ ExamenU3/src/Ventana.java
@@ -1,34 +1,21 @@
 import java.awt.Color;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Ventana {
-	
-	private JFrame frame;
-	private Imagen imagenes;
-	private String rutaRecursos = "Resources/";
-	public Ventana() {
-		inicializar();
-	}
+    private JFrame frame;
 
-	private void inicializar() {
-		frame = new JFrame();
-		frame.setVisible(true);
-		frame.setBounds(0, 0, 800, 500);
-		frame.setLocationRelativeTo(null);
-		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
-		panel.setBackground(Color.black);
-		frame.getContentPane().add(panel);
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    public Ventana() {
+        inicializar();
+    }
 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		imagenes = new Imagen(rutaRecursos + "fondo.png",panel);
-		
-		panel.repaint();
-		panel.revalidate();
-	}
+    private void inicializar() {
+        frame = new JFrame();
+        frame.setBounds(0, 0, 1280, 832);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setLayout(null);
+        frame.setBackground(Color.white);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);  
+    }
 }
