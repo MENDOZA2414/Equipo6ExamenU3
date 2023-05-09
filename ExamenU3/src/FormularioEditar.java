a5e45256f2992bb3988d20e56354d71fdf145539
4NG3LD4NI3L <90060617+4NG3LD4NI3L@users.noreply.github.com>
2023-05-09 01:28:41 -0600

    semicompleto

--- ExamenU3/src/FormularioEditar.java
+++ ExamenU3/src/FormularioEditar.java
@@ -15,52 +15,95 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.DocumentFilter.FilterBypass;
 
 public class FormularioEditar {
 	
 	private static final int NUM_IMAGENES = 1;
-	private JPanel panel;
+	
+	JPanel panel;
+	JPanel panelPrincipal;
+    JTextArea descripcion;
+    JScrollPane scrollPaneJArea;
+    String nombreImagen;
+    JButton aceptar;
+    JTextField nombre;
+    JTextField precio;
+    JComboBox<String> categoria;
 	
 	public FormularioEditar(JPanel panel){
 		
 		this.panel = panel;
-        panel.setBounds(0, 0, 1180, 732);       //MEDIDA DEL PANEL PRINCIPAL
-        panel.setBackground(Color.decode("#EBEBEB"));
-        panel.setLayout(null);
+		panel.setLayout(null);
+        panel.setBounds(250, 60, 500, 430);       //MEDIDA DEL PANEL PRINCIPAL
+        panel.setBackground(Color.decode("#C7C7C7"));
 
         JLabel tP = new JLabel("Editar Platillo",JLabel.CENTER);
-        tP.setBounds(90, 20, 120, 30);
-        tP.setForeground(Color.decode("#737373"));
+        tP.setBounds(150, 20, 120, 30);
+        tP.setForeground(Color.decode("#4D4D4D"));
         tP.setFont(new Font(" ",Font.BOLD, 18));
         panel.add(tP);
 
-        JTextField nombre = new JTextField("Nombre del platillo");
-        nombre.setBounds(57, 70, 185, 30);
+        nombre = new JTextField("Nombre del platillo");
+        nombre.setBounds(57, 70, 285, 30);
         nombre.setForeground(Color.decode("#737373"));
         panel.add(nombre);
 
-        JTextArea des = new JTextArea(" Agrega descripcion del platillo");
-        des.setBounds(57, 120, 185, 70);
-        des.setForeground(Color.decode("#737373"));
-        panel.add(des);
+        scrollPaneJArea = new JScrollPane();
+        scrollPaneJArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        descripcion = new JTextArea(" Agrega descripcion del platillo");
+        descripcion.setBounds(57, 110, 285, 90);
+        descripcion.setForeground(Color.decode("#737373"));
 
-        JComboBox<String> categoria = new JComboBox<>();
-        categoria.setBounds(57, 210, 185, 30);
+        scrollPaneJArea.setBounds(57, 110, 285, 90);
+        scrollPaneJArea.getViewport().setBackground(Color.WHITE);
+        scrollPaneJArea.getViewport().add(descripcion);
+        panel.add(scrollPaneJArea);
+
+        categoria = new JComboBox<>();
+        categoria.setBounds(57, 210, 285, 30);
         categoria.setForeground(Color.decode("#737373"));
         categoria.addItem("Bebidas");
         categoria.addItem("Alimentos");
         categoria.addItem("Postres");
         panel.add(categoria);
 
-        JTextField precio = new JTextField("Precio");
-        precio.setBounds(57, 260, 185, 30);
+        precio = new JTextField("");
+        AbstractDocument doc = (AbstractDocument) precio.getDocument();
+
+        doc.setDocumentFilter(new DocumentFilter() {
+            
+            @Override
+            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
+                    throws BadLocationException {
+                if (string.matches("[0-9]+")) {
+                    super.insertString(fb, offset, string, attr);
+                }
+            }
+        
+            @Override
+            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs)
+                    throws BadLocationException {
+                if (text.matches("[0-9]+")) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+
+        });
+
+        precio.setBounds(57, 260, 285, 30);
         precio.setForeground(Color.decode("#737373"));
         panel.add(precio);
 
         JButton agFoto = new JButton("Agregar foto");
-        agFoto.setBounds(57, 310, 185, 30);
+        agFoto.setBounds(57, 310, 285, 30);
         panel.add(agFoto);
         
         agFoto.addActionListener(new ActionListener() {
@@ -82,7 +125,8 @@ public class FormularioEditar {
 	                for (int i = 0; i < selectedFiles.length; i++) {
 	                    // Cargar la imagen seleccionada
 	                    BufferedImage image = ImageIO.read(selectedFiles[i]);
-
+	                    nombreImagen =  selectedFiles[i].getName();
+	                    
 	                    // Guardar la imagen en disco
 	                    File outputfile = new File("C:\\Users\\USER\\Pictures\\Nueva carpeta" + i + ".png");
 	                    ImageIO.write(image, "png", outputfile);
@@ -101,32 +145,116 @@ public class FormularioEditar {
         	
         });
 
-        JButton aceptar = new JButton("A C E P T A R");
-        aceptar.setBounds(30, 360, 110, 30);
+        aceptar = new JButton("A C E P T A R");
+        aceptar.setBounds(80, 360, 110, 30);
         aceptar.setOpaque(true);
         aceptar.setBackground(Color.green);
         panel.add(aceptar);
 
         JButton cancelar = new JButton("C A N C E L A R");
-        cancelar.setBounds(157, 360, 117, 30);
+        cancelar.setBounds(205, 360, 117, 30);
         cancelar.setOpaque(true);
         cancelar.setBackground(Color.red);
         panel.add(cancelar);
         
         JButton eliminar = new JButton(null, new ImageIcon("Resources/borrar.png"));
-        eliminar.setBounds(320, 170, 109, 109);
+        eliminar.setBounds(350, 120, 109, 109);
         eliminar.setOpaque(true);
-        eliminar.setBackground(Color.decode("#EBEBEB"));
+        eliminar.setBackground(Color.decode("#C7C7C7"));
+        eliminar.setBorderPainted(false);
         panel.add(eliminar);
         
+        eliminar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Agregar borrar platillo");
+				
+			}
+        	
+        });
+        
         JLabel borrar = new JLabel("Eliminar platillo",JLabel.CENTER);
-        borrar.setBounds(317, 285, 120, 30);
+        borrar.setBounds(350, 225, 120, 30);
         borrar.setForeground(Color.decode("#737373"));
         borrar.setFont(new Font(" ",Font.BOLD, 16));
         panel.add(borrar);
 
         panel.repaint();
     }
+	
+	public JButton getAceptar() {
+		return aceptar;
+	}
+	
+
+	public void agregarPanel(JPanel panelPrincipal) {
+    	this.panelPrincipal = panelPrincipal;
+    	panelPrincipal.add(panel);
+    	panelPrincipal.repaint();
+        panel.repaint();
+    }
+    
+    public void remover() {
+    	panelPrincipal.remove(panel);
+    	panelPrincipal.repaint();
+    }
+
+    public JPanel getPanel(){
+        return panel;
+    }
+
+	public JTextArea getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(JTextArea descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public JScrollPane getScrollPaneJArea() {
+		return scrollPaneJArea;
+	}
+
+	public void setScrollPaneJArea(JScrollPane scrollPaneJArea) {
+		this.scrollPaneJArea = scrollPaneJArea;
+	}
+
+	public String getNombreImagen() {
+		return nombreImagen;
+	}
+
+	public void setNombreImagen(String nombreImagen) {
+		this.nombreImagen = nombreImagen;
+	}
+
+	public void setAceptar(JButton aceptar) {
+		this.aceptar = aceptar;
+	}
+
+	public JTextField getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(JTextField nombre) {
+		this.nombre = nombre;
+	}
+
+	public JTextField getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(JTextField precio) {
+		this.precio = precio;
+	}
+
+	public JComboBox<String> getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(JComboBox<String> categoria) {
+		this.categoria = categoria;
+	}
 	
 	
 }
